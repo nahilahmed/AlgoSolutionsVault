488c9bb65693c9905ae19268026de363cdfd009c
Anil Bhasi <anil.bhasi@amnhealthcare.com>
2024-08-13 08:04:03 +0530

    010 valid paranthesis

--- C Sharp/011_valid_paranthesis.cs
+++ C Sharp/011_valid_paranthesis.cs
@@ -1,8 +1,3 @@
-
-global using System;
-using System.Collections.Generic;
-
-
 public bool isValid(string s)
 {
     int n = s.Length;
@@ -34,22 +29,5 @@ public bool isValid(string s)
     return 0 == stack.Count();
 }
 
-// int[] arr = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
-string s = "}";
-System.Console.WriteLine(isValid(s));
-// Customprint(isValid(s));
 
-
-//print array
-public void Customprint(int[] arr)
-{
-    Console.WriteLine("[" + string.Join(",", arr) + "]");
-}
-
-public void Customprint<T>(Stack<T> stack)
-{
-    Console.WriteLine("Stack contents: " + string.Join(", ", stack) + "");;
-}
-
-
-// Command to run above code "dotnet script Program.cs"
+// https://leetcode.com/problems/valid-parentheses/description/
