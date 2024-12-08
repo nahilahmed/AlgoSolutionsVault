eb8a88148653e73ce193f910c5145faae7eb5fda
nahilahmed <nahil757@gmail.com>
2024-12-08 15:41:17 +0530

    Pascal Triangle Setup Code

---
+++ Python/021_PascalsTriangle.py
@@ -0,0 +1,9 @@
+def generate_pascals_triangle(numRows):
+    # Write your solution here
+    pass
+
+if __name__ == "__main__":
+    # Test your function
+    numRows = int(input("Enter the number of rows for Pascal's Triangle: "))
+    result = generate_pascals_triangle(numRows)
+    print("Output:", result)
